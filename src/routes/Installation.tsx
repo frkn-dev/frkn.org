5a0ba058a07fe09c66a64ced16168c1c245e4203
Roman Lesnoy <romanlesnoy@outlook.com>
2022-08-06 19:58:22 +0300

    Add a Installation page component

---
+++ src/routes/Installation.tsx
@@ -0,0 +1,34 @@
+import { useContext, useState } from "react";
+
+import { DataContext } from "../App";
+import { content } from "../content/instructions";
+import Navbar from "../components/Navbar";
+import PlatformNavigation from "../components/PlatformNavigation";
+import Instructions from "../components/Instructions/Instructions";
+
+const Installation = () => {
+    const appContext = useContext(DataContext);
+    if (!appContext) return null;
+    const { lang } = appContext;
+
+    const [platform, setPlatform] = useState("windows");
+
+    function platformHandler(platform: string) {
+        setPlatform(platform);
+    }
+
+    return (
+        <div className="min-h-screen flex flex-col md:max-w-xl lg:max-w-6xl mx-auto">
+            <Navbar />
+            <main className="p-6">
+                <h1 className="font-semibold text-3xl lg:text-[40px] text-zinc-800 mt-6 whitespace-pre-wrap mb-12">
+                    {content.header[lang]}
+                </h1>
+                <PlatformNavigation onSetPlatform={platformHandler} activePlatform={platform}/>
+                <Instructions platform={platform}/>
+            </main>
+        </div>
+    );
+};
+
+export default Installation;
