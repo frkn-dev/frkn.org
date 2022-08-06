a87eb0bb936ab552abc6358e7c50e199b45dc9aa
Roman Lesnoy <romanlesnoy@outlook.com>
2022-08-06 21:08:56 +0300

    Fix Data Provider imports

--- src/components/Instructions/Screencast/Screencast.tsx
+++ src/components/Instructions/Screencast/Screencast.tsx
@@ -1,36 +1,31 @@
 import { useContext } from "react";
 
 import { content } from "../../../content/instructions";
-import { DataContext } from "../../../App";
+import { DataContext } from "../../../providers/DataProvider";
 
 const Screeencast: React.FC<{ platform: string }> = (props) => {
-    const appContext = useContext(DataContext);
-    if (!appContext) return null;
-    const { lang } = appContext;
+  const appContext = useContext(DataContext);
+  if (!appContext) return null;
+  const { lang } = appContext;
 
-    const { subtitle, text, textLink } = content.screencastSection;
-    const { url, preload, controls, poster, type } =
-        content.instructions[props.platform].video;
-    console.log("screencast");
-    return (
-        <div>
-            <h3 className="text-2xl font-semibold pb-2">{subtitle[lang]}</h3>
-            <video
-                key={url}
-                controls={controls}
-                preload={preload}
-                poster={poster}
-            >
-                <source src={url} type={type} />
-                <p>
-                    {text[lang]}
-                    <a href={url} download>
-                        {textLink[lang]}
-                    </a>
-                </p>
-            </video>
-        </div>
-    );
+  const { subtitle, text, textLink } = content.screencastSection;
+  const { url, preload, controls, poster, type } =
+    content.instructions[props.platform].video;
+  console.log("screencast");
+  return (
+    <div>
+      <h3 className="text-2xl font-semibold pb-2">{subtitle[lang]}</h3>
+      <video key={url} controls={controls} preload={preload} poster={poster}>
+        <source src={url} type={type} />
+        <p>
+          {text[lang]}
+          <a href={url} download>
+            {textLink[lang]}
+          </a>
+        </p>
+      </video>
+    </div>
+  );
 };
 
 export default Screeencast;
--- src/components/PlatformNavigation.tsx
+++ src/components/PlatformNavigation.tsx
@@ -1,56 +1,54 @@
 import { useContext } from "react";
-import { DataContext } from "../App";
+import { DataContext } from "../providers/DataProvider";
 import { content } from "../content/instructions";
 
 interface IPlatform {
-    id: string;
-    img: string;
-    title: string;
+  id: string;
+  img: string;
+  title: string;
 }
 
 const PlatformNavigation: React.FC<{
-    onSetPlatform: (platform: string) => void;
-    activePlatform: string;
+  onSetPlatform: (platform: string) => void;
+  activePlatform: string;
 }> = (props) => {
-    const appContext = useContext(DataContext);
-    if (!appContext) return null;
-    const { lang } = appContext;
+  const appContext = useContext(DataContext);
+  if (!appContext) return null;
+  const { lang } = appContext;
 
-    const { options } = content;
+  const { options } = content;
 
-    const liClassName =
-        "flex flex-auto lg:flex-initial bg-zinc-100 py-4 px-4 md:px-4 lg:pr-10 rounded-lg cursor-pointer hover:bg-zinc-200 transition-all duration-200 relative";
-    const liActiveClassName = "bg-zinc-300";
+  const liClassName =
+    "flex flex-auto lg:flex-initial bg-zinc-100 py-4 px-4 md:px-4 lg:pr-10 rounded-lg cursor-pointer hover:bg-zinc-200 transition-all duration-200 relative";
+  const liActiveClassName = "bg-zinc-300";
 
-    return (
-        <div className="flex flex-col md:max-w-xl lg:max-w-6xl mx-auto pb-6">
-            <h2 className="text-2xl font-semibold">
-                {content.platforms[lang]}
-            </h2>
-            <ul className="flex flex-col md:flex-row gap-4 mt-6 flex-wrap">
-                {options.map((platform: IPlatform) => {
-                    return (
-                        <li
-                            key={platform.id}
-                            onClick={() => props.onSetPlatform(platform.id)}
-                            className={
-                                props.activePlatform === platform.id
-                                    ? `${liClassName} ${liActiveClassName}`
-                                    : `${liClassName}`
-                            }
-                        >
-                            <img
-                                src={platform.img}
-                                alt={platform.title}
-                                className="w-6 h-6"
-                            />
-                            <p className="ml-4 text-lg">{platform.title}</p>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="flex flex-col md:max-w-xl lg:max-w-6xl mx-auto pb-6">
+      <h2 className="text-2xl font-semibold">{content.platforms[lang]}</h2>
+      <ul className="flex flex-col md:flex-row gap-4 mt-6 flex-wrap">
+        {options.map((platform: IPlatform) => {
+          return (
+            <li
+              key={platform.id}
+              onClick={() => props.onSetPlatform(platform.id)}
+              className={
+                props.activePlatform === platform.id
+                  ? `${liClassName} ${liActiveClassName}`
+                  : `${liClassName}`
+              }
+            >
+              <img
+                src={platform.img}
+                alt={platform.title}
+                className="w-6 h-6"
+              />
+              <p className="ml-4 text-lg">{platform.title}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default PlatformNavigation;
