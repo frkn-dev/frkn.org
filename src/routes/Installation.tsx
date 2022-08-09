3daea9bcf3da9077da525ee533dd4d15d4de1c4d
Roman Lesnoy <romanlesnoy@outlook.com>
2022-08-09 16:43:32 +0300

    Add scroll functionality to instruction article on mobile resolutions

--- src/routes/Installation.tsx
+++ src/routes/Installation.tsx
@@ -1,38 +1,52 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 
 import { DataContext } from "../providers/DataProvider";
 import { content } from "../content/instructions";
 import Navbar from "../components/Navbar";
 import PlatformNavigation from "../components/PlatformNavigation";
 import Instructions from "../components/Instructions/Instructions";
+import useWindowDimensions from "../hooks/useDementions";
+
+const MOBILE_BREACKPOINT = 768;
 
 const Installation = () => {
   const appContext = useContext(DataContext);
   if (!appContext) return null;
   const { lang } = appContext;
 
+  const { width: screenWidth } = useWindowDimensions();
+
   const [platform, setPlatform] = useState<string>(getOS());
+  const articleRef = useRef<HTMLElement>(null);
 
   function getOS(): string {
     let userAgent = window.navigator.userAgent,
-        platform = window.navigator.platform,
-        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-        iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+      platform = window.navigator.platform,
+      macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+      windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+      iosPlatforms = ["iPhone", "iPad", "iPod"];
 
     if (macosPlatforms.indexOf(platform) !== -1) {
-      return  'macos';
+      return "macos";
     } else if (iosPlatforms.indexOf(platform) !== -1) {
-      return  'ios';
+      return "ios";
     } else if (windowsPlatforms.indexOf(platform) !== -1) {
-      return  'windows';
+      return "windows";
     } else if (/Android/.test(userAgent)) {
-      return  'android';
-    } else return 'linux';
+      return "android";
+    } else return "linux";
   }
 
   function platformHandler(platform: string) {
     setPlatform(platform);
+    if (screenWidth < MOBILE_BREACKPOINT) {
+      scrollCallback();
+    }
+  }
+
+  function scrollCallback() {
+    if (!articleRef.current) throw Error("articleRef is not assigned");
+    articleRef.current.scrollIntoView({ behavior: "smooth" });
   }
 
   return (
@@ -46,7 +60,7 @@ const Installation = () => {
           onSetPlatform={platformHandler}
           activePlatform={platform}
         />
-        <Instructions platform={platform} />
+        <Instructions platform={platform} refProps={articleRef} />
       </main>
     </div>
   );
