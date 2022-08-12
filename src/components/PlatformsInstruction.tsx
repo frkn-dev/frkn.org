fa1652691f1f59f723f50470ee417bc3d26f95ef
Alexander Kislyakov <kislyakov.sk@yandex.ru>
2022-08-13 01:37:11 +0300

    fix: mobile scroll to element on page `Instruction`

--- src/components/PlatformsInstruction.tsx
+++ src/components/PlatformsInstruction.tsx
@@ -6,27 +6,23 @@ import Instructions from './Instructions/Instructions';
 import useWindowDimensions from "../hooks/useDementions";
 
 import getOS from "../utils/getOS";
+import scrollToElement from "../utils/scrollToElement";
 
 const MOBILE_BREACKPOINT = 768;
 
 const PlatformsInstruction = () => {
     const { width: screenWidth } = useWindowDimensions();
 
-    const [platform, setPlatform] = useState<string>(getOS());
+    const [platform, setPlatform] = useState(getOS());
     const articleRef = useRef<HTMLElement>(null);
 
     const platformHandler = useCallback((platform: string) => {
         setPlatform(platform);
 
-        if (screenWidth < MOBILE_BREACKPOINT) {
-            scrollCallback();
+        if (screenWidth < MOBILE_BREACKPOINT && articleRef.current) {
+            scrollToElement(articleRef.current);
         }
-    }, []);
-
-    function scrollCallback() {
-        if (!articleRef.current) throw Error("articleRef is not assigned");
-        articleRef.current.scrollIntoView({ behavior: "smooth" });
-    }
+    }, [screenWidth]);
 
     return (
         <>
---
+++ src/utils/scrollToElement.ts
@@ -0,0 +1,5 @@
+function scrollToElement(element: HTMLElement) {
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+export default scrollToElement;
