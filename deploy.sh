9e64b475d6858acebe858a764d13c81364f33684
Kira Sotnikoff <the2pizza@proton.me>
2022-08-19 22:47:25 +0400

    Add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,10 @@
 # abort on errors
 set -e
 
-# build
-yarn install 
-yarn build
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+yarn install && yarn build
+mkdir -p ../deploy
+cp -R dist/* ../deploy
+git checkout pages
+cp -R ../deploy/* .
+git add -A && git commit -S -m "deploy"
+git push
