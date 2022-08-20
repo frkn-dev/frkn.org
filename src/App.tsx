1a8986a20dc3bfb00b6436195b51dd7b61fa05d1
0xTovand <tovand@protonmail.com>
2022-08-20 23:17:08 +0300

    hashrouter instead of browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Donate from './routes/Donate';
 import About from './routes/About';
 import NotFound from './routes/NotFound';
-import {DataProvider} from "./providers/DataProvider";
+import { DataProvider } from './providers/DataProvider';
 import Installation from './routes/Installation';
 
 function App() {
-
   return (
     <DataProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="support" element={<Donate />} />
           <Route path="about" element={<About />} />
-          <Route path="install" element={<Installation />} /> 
+          <Route path="install" element={<Installation />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </DataProvider>
   );
 }
