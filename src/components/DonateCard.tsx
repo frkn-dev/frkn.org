e6e2b957284012754d5c502ab9bb6b75ffdcd0b8
0xTovand <tovand@protonmail.com>
2022-08-20 21:40:59 +0300

    fixed tooltips that didn't show up in prod

--- src/components/DonateCard.tsx
+++ src/components/DonateCard.tsx
@@ -1,6 +1,4 @@
-import Tippy from '@tippyjs/react';
-import { useContext, useRef } from 'react';
-import 'tippy.js/dist/tippy.css';
+import { useContext, useRef, useState } from 'react';
 import { content } from '../content/donate';
 import { DataContext } from '../providers/DataProvider';
 
@@ -12,13 +10,16 @@ interface CardProps {
   address?: string;
 }
 
-const DonateCard = ({ img, title, text, url, address }: CardProps) => {
+const DonateCard = ({ img, title, url, address }: CardProps) => {
   const appContext = useContext(DataContext);
   if (!appContext) return null;
   const { lang } = appContext;
+  const [visible, setVisible] = useState(false);
+  const [copied, setCopied] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   const copyToClipboard = () => {
     if (address) navigator.clipboard.writeText(address);
+    setCopied(true);
   };
 
   return (
@@ -26,30 +27,32 @@ const DonateCard = ({ img, title, text, url, address }: CardProps) => {
       onClick={copyToClipboard}
       className="flex flex-auto lg:flex-initial bg-zinc-100 py-6 px-4 md:px-8 lg:pr-10 rounded-lg cursor-pointer hover:bg-zinc-200 transition-all duration-200 relative"
       ref={ref}
+      onMouseEnter={() => setVisible(true)}
+      onMouseLeave={() => {
+        setVisible(false);
+        setCopied(false);
+      }}
     >
       <img src={img} alt="" className="w-12 h-12" />
       <div className="ml-4 flex flex-col justify-center ">
         <h3 className="text-lg">{title}</h3>
-        {/* <p className="text-zinc-500 text-sm">{text}</p> */}
       </div>
       {address && (
-        <>
-          <Tippy content={content.copy[lang]} reference={ref} />
-          <Tippy
-            content={content.copied[lang]}
-            reference={ref}
-            trigger="click"
-            onShow={(instance) => {
-              setTimeout(() => {
-                instance.hide();
-              }, 1000);
-            }}
-          />
-        </>
+        <div
+          className={`absolute top-[-40px] text-sm p-2 px-4 bg-zinc-900 text-zinc-50 left-1/2 -translate-x-1/2 w-fit whitespace-nowrap rounded-lg transition-all duration-200 ${
+            visible ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
+          {copied ? content.copied[lang] : content.copy[lang]}
+        </div>
       )}
       {url && (
-        <a href={url} className="absolute top-0 left-0 w-full h-full" target="_blank"
-        rel="noopener noreferrer"></a>
+        <a
+          href={url}
+          className="absolute top-0 left-0 w-full h-full"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
       )}
     </article>
   );
