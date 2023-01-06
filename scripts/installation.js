de438781fe80d2cf4b849609ae5295367e7c3a94
wanku-x <wanku@bk.ru>
2023-01-06 15:45:13 +0400

    Add form to download WG config && QR code with WG config

--- scripts/installation.js
+++ scripts/installation.js
@@ -2,11 +2,11 @@ const Select = document.querySelector('.installation__select')
 const Link = document.querySelector('.installation__link')
 const Image = document.querySelector('.installation__qr')
 
-const createConfig = ({ interface, peer }) => (
+const createConfig = ({ iface, peer }) => (
 `[Interface]
-Address = ${interface.address}
-DNS = ${interface.dns}
-PrivateKey = ${interface.key}
+Address = ${iface.address}
+DNS = ${iface.dns}
+PrivateKey = ${iface.key}
 
 [Peer]
 PublicKey = ${peer.pubkey}
@@ -18,7 +18,7 @@ PersistentKeepalive = 25`
 
 const createURI = (text) => `data:text/plain;charset=utf-8,${encodeURIComponent(createConfig(text))}`
 
-fetch('http://94.176.238.220:8080/locations')
+fetch('https://api.fuckrkn1.xyz/locations')
     .then((res) => res.json())
     .then((data) => {
         data.forEach((server) => {
@@ -31,7 +31,7 @@ fetch('http://94.176.238.220:8080/locations')
         })
     })
 
-fetch('http://94.176.238.220:8080/peer')
+fetch('https://api.fuckrkn1.xyz/peer')
     .then((res) => res.json())
     .then((data) => {
         Link.setAttribute('href', createURI(data))
@@ -51,5 +51,30 @@ fetch('http://94.176.238.220:8080/peer')
     })
 
 Select.addEventListener('change', () => {
-    console.log(Select.value)
+    Link.removeAttribute('href')
+    Link.removeAttribute('download')
+    Image.setAttribute('src', '/Images/qr-placeholder.svg')
+
+    const url = Select.value.length
+        ? `https://api.fuckrkn1.xyz/peer?location=${Select.value}`
+        : 'https://api.fuckrkn1.xyz/peer'
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            Link.setAttribute('href', createURI(data))
+            Link.setAttribute('download', 'fuckrkn1.conf')
+        
+            const qrcode = new QRCode({
+                content: createConfig(data),
+                padding: 0,
+                width: 256,
+                height: 256,
+                color: "#000000",
+                background: "transparent",
+                ecl: "M",
+            })
+            
+            Image.setAttribute('src', `data:image/svg+xml;utf8,${encodeURIComponent(qrcode.svg())}`)
+        })
 })
