862fa1f80d53d4a235e10c552834d39c20b438b4
Sergey Komarov <komarovs33@mail.ru>
2023-06-04 16:49:09 +0300

    feat: Moved wallets values to wallets.js

---
+++ scripts/wallets.js
@@ -0,0 +1,7 @@
+const wallets = {
+    btc: "bc1q5xjm7v20aee4tl2vngqju9k0uzajl2hvljqec6",
+    ethereum: "0x92A3Fad82F27938AA0D5c7509132DD781C4880bb",
+};
+
+document.getElementById("btc").innerHTML = wallets.btc;
+document.getElementById("ethereum").innerHTML = wallets.ethereum;
