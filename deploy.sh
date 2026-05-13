e7b4efa4152db1899ed818038700c3e22dbf42d3
Fyodor Sotnikov <s8tn1kv@gmail.com>
2026-05-13 14:17:09 +0300

    fix plain links

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+SERVER=$1
+
+rsync -avz --delete \
+  --exclude='.git' \
+  --exclude='.DS_Store' \
+  --exclude='.github' \
+  --exclude='.gitignore' \
+  --exclude='CNAME' \
+  --exclude='LICENSE.txt' \
+  -e "ssh" \
+  ./ ${SERVER}:/opt/mirror/frkn.org/
