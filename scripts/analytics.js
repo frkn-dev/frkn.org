fd0e2c9d5a0fb20768f62f808be42355caa10dbc
Fyodor Sotnikov <s8tn1kv@gmail.com>
2026-07-11 00:39:20 +0300

    fix analitycs script

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,9 +1,38 @@
 (function () {
   var img = new Image();
   var params = [];
-  params.push('page=' + encodeURIComponent(location.pathname + location.search));
+
+  params.push(
+    "page=" + encodeURIComponent(location.pathname + location.search),
+  );
+  params.push("host=" + encodeURIComponent(location.host));
+
   if (document.referrer) {
-    params.push('ref=' + encodeURIComponent(document.referrer));
+    params.push("ref=" + encodeURIComponent(document.referrer));
   }
-  img.src = 'https://media.frkn.org/pixel?' + params.join('&');
+
+  var lang = navigator.language || navigator.userLanguage || "";
+  if (lang) {
+    params.push("lang=" + encodeURIComponent(lang));
+  }
+
+  function getQueryParam(name) {
+    var match = new RegExp("[?&]" + name + "=([^&]*)").exec(location.search);
+    return match ? decodeURIComponent(match[1].replace(/\+/g, " ")) : "";
+  }
+
+  [
+    "utm_source",
+    "utm_medium",
+    "utm_campaign",
+    "utm_content",
+    "utm_term",
+  ].forEach(function (key) {
+    var value = getQueryParam(key);
+    if (value) {
+      params.push(key + "=" + encodeURIComponent(value));
+    }
+  });
+
+  img.src = "https://media.frkn.org/pixel?" + params.join("&");
 })();
