a5ce7cba6514f98cf176fad7f24bf39cad314f8b
Fyodor Sotnikov <s8tn1kv@gmail.com>
2026-08-19 21:40:55 +0300

    conflicts

--- frkn-app/scripts/analytics.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  var img = new Image();
-  var params = [];
-  params.push('page=' + encodeURIComponent(location.pathname + location.search));
-  if (document.referrer) {
-    params.push('ref=' + encodeURIComponent(document.referrer));
-  }
-  img.src = 'https://media.frkn.org/pixel?' + params.join('&');
-})();
